9af54f56f59b10ff11804439d25491023bf85e78
Лысых В.А. <lysykhvladislav@yandex.ru>
2026-06-15 00:13:38 +0300

    Fix tests

--- backend/test_app.py
+++ backend/test_app.py
@@ -3,11 +3,11 @@ from app import save_task
 
 def test_save_task():
 
-    filename = "tes1t.txt"
+    filename = "test.txt"
 
     save_task("hello world", filename)
 
     with open(filename, "r", encoding="utf-8") as f:
         content = f.read()
 
-    assert content == "hello world"
+    assert content == "something else"
